57262af5a7c4e11daee9d6b194e8c2b83b5b7a0e
Jessica Mondragón <jessimm97@gmail.com>
2021-11-04 11:50:27 -0600

    primer cambio

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+alert("Bienbenidas Mujeres emprendedoras");
+confirm("¿Deseas saber acerca de autonomía económica de las mujeres?");
+
+var person = prompt("Ingresa tu nombre");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hola " + person + "Bienvenida";
+}https://github.com/Jessimm97/MWT2_AEM.git
+
